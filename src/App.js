0c55e5d9125e674988991631a16a15578d9d2265
kamlesh <kamlesh.kumawat@axa-abs-co.in>
2023-01-12 23:50:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useDebounce } from './hook/useDebounce';
+import { useState } from 'react'
 function App() {
+
+  const [value, setValue] = useState(0)
+
+  const debouncedValue = useDebounce(value, 2000)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Debouncing <code>React.js</code> and Custom Hook.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="App-link">Current Value: {value} | Debounced Value: {debouncedValue}</p>
+        <button className='button' type='button' onClick={() => setValue(value+1)}>Count</button>
       </header>
     </div>
   );
---
+++ src/hook/useDebounce.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react"
+
+export const useDebounce = (value, delay) => {
+    const [state, setState] = useState(value);
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setState(value);
+        }, delay);
+
+        return () => {
+            clearTimeout(timerId)
+        }
+    })
+    return state
+}
